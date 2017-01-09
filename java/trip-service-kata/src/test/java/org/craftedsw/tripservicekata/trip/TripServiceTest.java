779b37075d4fc8ac4d486ed0db175fac3513740d
unknown <XF76@INLTD09921.global.tesco.org>
2017-01-09 23:35:19 +0530

    RED - trying to add test case, as we are not suppose to change code which is not covered by unit test cases

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,30 @@
 package org.craftedsw.tripservicekata.trip;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Before;
+import org.junit.Test;
 public class TripServiceTest {
+	
+	private TripService tripService;
+	
+	@Before
+	public void setup(){
+		tripService = new TripService();
+	}
+	
+	@Test
+	public void should_return_trips_for_valid_user(){
+		assertThat(tripService.getTripsByUser(createUser()).size(), is(1));
+	}
+
+	private User createUser() {
+		User user = new User();
+		User friend = new User();
+		user.addFriend(friend);
+		user.addTrip( new Trip());
+		return user;
+	}
 	
 }
